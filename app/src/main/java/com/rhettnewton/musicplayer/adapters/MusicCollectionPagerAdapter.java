87ab33a9f016fa87bd1eece24b894bc6d46fe3c0
Rhett <rhett.newton@gmail.com>
2018-03-06 09:12:00 -0600

    Change root element to ViewPager

---
+++ app/src/main/java/com/rhettnewton/musicplayer/adapters/MusicCollectionPagerAdapter.java
@@ -0,0 +1,10 @@
+package com.rhettnewton.musicplayer.adapters;
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Rhett on 3/5/2018.
+ */
+
+public class MusicCollectionPagerAdapter extends FragmentPagerAdapter {
+}
