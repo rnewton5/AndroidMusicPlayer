7011af9549b57b1943741f6b7bf236627c1033c9
Rhett <rhett.newton@gmail.com>
2018-03-07 09:32:29 -0600

    Add AlbumViewActivity

---
+++ app/src/main/java/com/rhettnewton/musicplayer/activities/AlbumViewActivity.java
@@ -0,0 +1,15 @@
+package com.rhettnewton.musicplayer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rhettnewton.musicplayer.R;
+
+public class AlbumViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_view);
+    }
+}
