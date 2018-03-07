2458d4ceb0f9f0b81a6cedbfe6363458ab117548
Rhett <rhett.newton@gmail.com>
2018-03-07 09:33:33 -0600

    Add ArtistViewActivity

---
+++ app/src/main/java/com/rhettnewton/musicplayer/activities/ArtistViewActivity.java
@@ -0,0 +1,15 @@
+package com.rhettnewton.musicplayer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rhettnewton.musicplayer.R;
+
+public class ArtistViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_view);
+    }
+}
