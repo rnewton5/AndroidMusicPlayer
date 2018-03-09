cfb76e9627335ddee483f8380e11ce20cf5707b5
Rhett <rhett.newton@gmail.com>
2018-03-09 10:51:13 -0600

    Fix seconds bug

--- app/src/main/java/com/rhettnewton/musicplayer/utils/TimeUtils.java
+++ app/src/main/java/com/rhettnewton/musicplayer/utils/TimeUtils.java
@@ -9,6 +9,9 @@ public class TimeUtils {
     public static String convertMillisToMinutesSeconds(long millis) {
         long minutes = (millis / 1000)  / 60;
         long seconds = (millis / 1000) % 60;
+        if (seconds < 10) {
+            return String.valueOf(minutes) + ":0" + String.valueOf(seconds);
+        }
         return String.valueOf(minutes) + ":" + String.valueOf(seconds);
     }
 }
