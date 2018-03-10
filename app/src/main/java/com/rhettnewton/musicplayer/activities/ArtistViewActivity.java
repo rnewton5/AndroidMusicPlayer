9efabf34e7536601625bdd6e02d48f193210fa17
Rhett <rhett.newton@gmail.com>
2018-03-10 09:35:17 -0600

    Add public extra fields

--- app/src/main/java/com/rhettnewton/musicplayer/activities/ArtistViewActivity.java
+++ app/src/main/java/com/rhettnewton/musicplayer/activities/ArtistViewActivity.java
@@ -7,6 +7,9 @@ import com.rhettnewton.musicplayer.R;
 
 public class ArtistViewActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ARTIST_ID = "extra_artist_it";
+    public static final String EXTRA_ARTIST_NAME = "extra_artist_name";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
